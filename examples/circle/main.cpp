d25652db7f3f4102b30583f4fcc3b424da6d38cd
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2024-01-11 12:14:50 -0600

    Improved output of circle.

--- examples/circle/main.cpp
+++ examples/circle/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 int main()
 {
    cout << showpoint << fixed << setprecision(2);
+
+   cout << "Defining a circle with radius 3" << endl;
    Circle c1(3);
    cout << left << setw(14) << "radius" 
         << right << setw(10) << c1.radius() << endl;
@@ -23,7 +25,12 @@ int main()
         << right << setw(10) << c1.circumference() << endl;
    cout << endl;
 
+   cout << "Defining a circle then setting radius to 2" << endl;
    Circle c2;
+   cout << "Before setting radius to 2" << endl;
+   cout << left << setw(14) << "radius" 
+        << right << setw(10) << c2.radius() << endl;
+   cout << "After setting radius to 2" << endl;
    c2.setRadius(2);
    cout << left << setw(14) << "radius" 
         << right << setw(10) << c2.radius() << endl;
