885ad87ef9dfebbfc76077139b45d71742276b66
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2024-01-25 11:43:48 -0600

    Add new directory for circle_complete

---
+++ examples/circle_complete/main.cpp
@@ -0,0 +1,46 @@
+// File:     main.cpp
+// Author:   Dan Brekke with tweaks by Juan Cabanela
+
+// Description
+// This program will test the circle class
+
+#include <iostream>
+#include <iomanip>
+#include "circle.h"
+using namespace std;
+
+int main()
+{
+   cout << showpoint << fixed << setprecision(2);
+
+   cout << "Defining a circle with radius 3" << endl;
+   Circle c1(3);
+   cout << left << setw(14) << "radius" 
+        << right << setw(10) << c1.radius() << endl;
+   cout << left << setw(14) << "diameter" 
+        << right << setw(10) << c1.diameter() << endl;
+   cout << left << setw(14) << "area" 
+        << right << setw(10) << c1.area() << endl;
+   cout << left << setw(14) << "circumference" 
+        << right << setw(10) << c1.circumference() << endl;
+   cout << endl;
+
+   cout << "Defining a circle then setting radius to 2" << endl;
+   Circle c2;
+   cout << "Before setting radius to 2" << endl;
+   cout << left << setw(14) << "radius" 
+        << right << setw(10) << c2.radius() << endl;
+   cout << "After setting radius to 2" << endl;
+   c2.setRadius(2);
+   cout << left << setw(14) << "radius" 
+        << right << setw(10) << c2.radius() << endl;
+   cout << left << setw(14) << "diameter" 
+        << right << setw(10) << c2.diameter() << endl;
+   cout << left << setw(14) << "area" 
+        << right << setw(10) << c2.area() << endl;
+   cout << left << setw(14) << "circumference" 
+        << right << setw(10) << c2.circumference() << endl;
+   cout << endl;
+
+   return 0;
+}
