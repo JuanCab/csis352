856a123b1ce8119009c611c6dd6655fe7887b0b3
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2024-02-19 16:51:43 -0600

    Cleaned up stl_inheritance.cpp

--- examples/stl_inheritance.cpp
+++ examples/stl_inheritance.cpp
@@ -2,61 +2,60 @@
 #include <list>
 using namespace std;
 
-void visit(int& value)
+void visit(int &value)
 {
-   cout << value << ' ';
+    cout << value << ' ';
 }
 
 template <class T>
 class sortedList : public list<T>
 {
-   public:
-      void insert(const T&);
-      void print() const;
-      void traverse(void (*) (T&));
+public:
+    void traverse(void (*)(T &));
+    void print() const;
+    void insert(const T &);
 };
 
 template <class T>
-void sortedList<T>::traverse(void (*funcPtr) (T&))
+void sortedList<T>::traverse(void (*funcPtr)(T &))
 {
-   typename list<T>::iterator p;
-   for (p=list<T>::begin(); p!=list<T>::end(); p++)
-      funcPtr(*p);
+    typename list<T>::iterator it;
+    for (it = list<T>::begin(); it != list<T>::end(); it++)
+        funcPtr(*it);
 }
 
 template <class T>
 void sortedList<T>::print() const
 {
-   typename list<T>::const_iterator p;
-   for (p=list<T>::begin(); p!=list<T>::end(); p++)
-      cout << *p << ' ';
-   cout << endl;
+    typename list<T>::const_iterator it;
+    for (it = list<T>::begin(); it != list<T>::end(); it++)
+        cout << *it << ' ';
+    cout << endl;
 }
 
 template <class T>
-void sortedList<T>::insert(const T& newitem)
+void sortedList<T>::insert(const T &newitem)
 {
-   typename list<T>::iterator p;
-   p = list<T>::begin();
-   while (p!=list<T>::end() && newitem>*p)
-      p++;
-   list<T>::insert(p,newitem);
+    typename list<T>::iterator it;
+    it = list<T>::begin();
+    while (it != list<T>::end() && newitem > *it)
+        it++;
+    list<T>::insert(it, newitem);
 }
-
 
 int main()
 {
-   sortedList<int> x;
-   x.insert(6);
-   x.insert(2);
-   x.insert(8);
-   x.insert(5);
-   if (x.empty())
-      cout << "empty" << endl;
-   else
-      cout << "not empty" << endl;
-   x.print();
-   x.traverse(visit);
-   cout << endl;
-   return 0;
+    sortedList<int> x;
+    x.insert(6);
+    x.insert(2);
+    x.insert(8);
+    x.insert(5);
+    if (x.empty())
+        cout << "empty" << endl;
+    else
+        cout << "not empty" << endl;
+    x.print();
+    x.traverse(visit);
+    cout << endl;
+    return 0;
 }
